963f9edab064e23f0d5bf2c034623673cdc9978b
Skeindem <117597448+Skeindem@users.noreply.github.com>
2025-07-23 20:26:37 +0300

    Delete SnailWebsite2 directory
    Just trying to understand how to make GitHub pages work

--- SnailWebsite2/scripts/snail-creation.js
+++
@@ -1,6 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const snailName = params.get('snailName')
-
-if (snailName) {
-  document.getElementById('snailNamed').textContent = snailName;
-  }
